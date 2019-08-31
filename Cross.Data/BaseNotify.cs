41086e9fbf6b6b818d07061825e80ef4428a7561
HideLord <polimonom@gmail.com>
2019-09-01 01:15:36 +0300

    Program skeleton

---
+++ Cross.Data/BaseNotify.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cross.Data
+{
+    public abstract class BaseNotify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender,e) => { };
+    }
+}
---
+++ UserControls/EditorControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Cross.UserControls
+{
+    /// <summary>
+    /// Interaction logic for EditorControl.xaml
+    /// </summary>
+    public partial class EditorControl : UserControl
+    {
+        private const double delta = 5;
+
+        private static readonly DependencyProperty headerProp =
+            DependencyProperty.Register("Header", typeof(string), typeof(EditorControl));
+
+        private static readonly DependencyProperty bindingProp =
+            DependencyProperty.Register("EditedVal", typeof(double), typeof(EditorControl));
+
+        public string Header
+        {
+            get=> (string)GetValue(headerProp);
+            set=> SetValue(headerProp,value);
+        }
+
+        public double EditedVal
+        {
+            get => (double)GetValue(bindingProp);
+            set => SetValue(bindingProp, value);
+        }
+
+        public EditorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Decrease(object sender, RoutedEventArgs e)
+        {
+            EditedVal -= delta;
+        }
+        private void Increase(object sender, RoutedEventArgs e)
+        {
+            EditedVal += delta;
+        }
+    }
+}
